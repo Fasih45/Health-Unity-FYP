534af38f499f520d2157f5fbc0caf3c822f2bd75
abdulmoqeet21 <abdulmooqeetahsan@gmail.com>
2023-11-22 17:40:09 +0500

    cors updation

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const connectDB = require('./connection/connectionstring'); 
 const userRoutes = require('./routes/userRoutes');
 const bodyParser = require('body-parser');
-
+const cors = require("cors")
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const jwtSecret = process.env.JWT_SECRET;
 
 // Connect to MongoDB
 connectDB();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 // Routes
 app.use('/users', userRoutes);
 

a8c4ad923985fa990d258855f1d8871d70e81732
abdulmoqeet21 <abdulmooqeetahsan@gmail.com>
2023-12-05 03:18:39 +0500

    Getting profile of user doc

--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profileController');
 
-router.post('/docter/register',profileController.registerDoctorProfile );
-router.get('/docter/:username', profileController.getDoctorProfile);
+router.post('/doctor/register',profileController.registerDoctorProfile );
+router.get('/doctor/:username', profileController.getDoctorProfile);
 
 module.exports = router;

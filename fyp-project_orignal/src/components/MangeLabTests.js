e7208d3aed799e1ec20cbe995e49d1ec328f8fca
ZaainAli <zainalicht7510@gmail.com>
2024-03-24 01:54:20 +0500

    Final Page of Lab Mangement

--- fyp-project_orignal/src/components/MangeLabTests.js
+++ fyp-project_orignal/src/components/MangeLabTests.js
@@ -476,7 +476,7 @@ const MangeLabTests = () => {
           </button>}
           {allowaddtest ? (
             <button
-              type="button"
+              type="button " 
               onClick={handleSubmit}
               class="text-white mt-2 bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
             >

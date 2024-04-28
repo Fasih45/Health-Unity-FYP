876a6c8e7ac3af77f0e6a82e834674adfc9fa413
abdulmoqeet21 <abdulmooqeetahsan@gmail.com>
2024-04-29 01:40:52 +0500

    rating api all set

--- fyp-project_orignal/src/components/TableBody.js
+++ fyp-project_orignal/src/components/TableBody.js
@@ -7,6 +7,8 @@ import { Rate } from "antd";
 import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 import WritePriscription from "./WritePriscription";
 import "./StarStyle.css";
+import { rate } from "../redux/actions/docProfile";
+import { useDispatch } from "react-redux";
 const TableBody = ({
   appointments,
   viewprofile,
@@ -20,29 +22,19 @@ const TableBody = ({
   const [data, setData] = useState(null);
   const [Rating, setRating] = useState(0);
   const [showRating, setshowRating] = useState(false);
+  const [aID, setId] = useState(null);
   const [Hover, setHover] = useState(null);
   const handleonclose = () => setshowmymodel(false);
-  const [userRatings, setUserRatings] = useState([]);
-  const updateUserRating = (username, rating) => {
-    setUserRatings(prevRatings => ({
-      ...prevRatings,
-      [username]: rating
-    }));
-  };
 
-
-  useEffect(() => {
-    console.log("userRating", userRatings);
-  }, [userRatings]);
+  const dispatch = useDispatch();
   useEffect(() => {
     console.log("Data Appointment :", appointments);
   }, []);
 
-
   return (
     <>
       <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-        {appointments.map((appointment) => (
+        {appointments.map((appointment, index) => (
           <tr key={appointment.id}>
             <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
               {appointment.date.split("T")[0]}
@@ -50,29 +42,31 @@ const TableBody = ({
             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
               <span
                 class={`relative inline-block px-3 py-1 font-semibold
-              ${appointment.status === "Settled"
-                    ? "text-blue-900"
-                    : appointment.status === "Approved"
-                      ? "text-green-900"
-                      : appointment.status === "Pending"
-                        ? "text-orange-900"
-                        : appointment.status === "Rejected"
-                          ? "text-red-900"
-                          : ""
-                  }  leading-tight`}
+              ${
+                appointment.status === "Settled"
+                  ? "text-blue-900"
+                  : appointment.status === "Approved"
+                  ? "text-green-900"
+                  : appointment.status === "Pending"
+                  ? "text-orange-900"
+                  : appointment.status === "Rejected"
+                  ? "text-red-900"
+                  : ""
+              }  leading-tight`}
               >
                 <span
                   aria-hidden
-                  className={`absolute inset-0 ${appointment.status === "Settled"
-                    ? "bg-blue-300" :
-                    appointment.status === "Approved"
+                  className={`absolute inset-0 ${
+                    appointment.status === "Settled"
+                      ? "bg-blue-300"
+                      : appointment.status === "Approved"
                       ? "bg-green-300"
                       : appointment.status === "Pending"
-                        ? "bg-orange-300"
-                        : appointment.status === "Rejected"
-                          ? "bg-red-300"
-                          : ""
-                    } opacity-50 rounded-full`}
+                      ? "bg-orange-300"
+                      : appointment.status === "Rejected"
+                      ? "bg-red-300"
+                      : ""
+                  } opacity-50 rounded-full`}
                 ></span>
 
                 <span class="relative">{appointment.status}</span>
@@ -116,7 +110,8 @@ const TableBody = ({
             )}
 
             <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-              {appointment.status === "Approved" || appointment.status === "Settled"
+              {appointment.status === "Approved" ||
+              appointment.status === "Settled"
                 ? appointment.timing
                 : "Not Assigned"}
             </td>
@@ -132,17 +127,19 @@ const TableBody = ({
                   >
                     View Doctor Profile
                   </button>
-                  {appointment.status === "Settled" ? (
+                  {appointment.status === "Settled" && !appointment.rate ? (
                     <button
                       className="text-blue-900 bg-blue-300 rounded-full relative inline-block px-3 py-1 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-300 hover:text-indigo-500 focus:outline-none"
                       onClick={() => {
                         setshowRating(true);
+                        setId({ id: appointment._id, index: index });
                       }}
                     >
                       Give Rating
                     </button>
-                  ) : <></>}
-
+                  ) : (
+                    <></>
+                  )}
                 </div>
               </td>
             )}
@@ -188,60 +185,141 @@ const TableBody = ({
           className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center"
         >
           <div className="bg-white p-5 w-20em pt-2 rounded ">
-
             <div className="grid grid-cols-6 gap-2 ">
               <div className="col-start-2 col-span-4">
                 <h1 className="font-semibold text-center text-xl text-gray-700 p-3">
                   {/* {appointments.doctorName} */}
                   Give Rating
-
                 </h1>
               </div>
               <div className="flex justify-end ">
-                <button type="button" 
-                onClick={() => {setshowRating(false);} }
-                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setshowRating(false);
+                    setRating(0);
+                  }}
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                >
                   <span class="sr-only">Close menu</span>
 
-                  <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                  <svg
+                    class="h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
                   </svg>
                 </button>
               </div>
-
             </div>
-
 
             <div className="flex justify-center mt-6 m-5">
               <>
                 <fieldset class="rate">
-                  <input type="radio" id="rating10" name="rating" value="10" onClick={() => setRating(5)} /><label for="rating10" title="5 stars"></label>
-                  <input type="radio" id="rating9" name="rating" value="9" onClick={() => setRating(4.5)} /><label class="half" for="rating9" title="4.5 stars"></label>
-                  <input type="radio" id="rating8" name="rating" value="8" onClick={() => setRating(4)} /><label for="rating8" title="4 stars"></label>
-                  <input type="radio" id="rating7" name="rating" value="7" onClick={() => setRating(3.5)} /><label class="half" for="rating7" title="3.5 stars"></label>
-                  <input type="radio" id="rating6" name="rating" value="6" onClick={() => setRating(3)} /><label for="rating6" title="3 stars"></label>
-                  <input type="radio" id="rating5" name="rating" value="5" onClick={() => setRating(2.5)} /><label class="half" for="rating5" title=" 2.5stars"></label>
-                  <input type="radio" id="rating4" name="rating" value="4" onClick={() => setRating(2)} /><label for="rating4" title="2 stars"></label>
-                  <input type="radio" id="rating3" name="rating" value="3" onClick={() => setRating(1.5)} /><label class="half" for="rating3" title="1.5 stars"></label>
-                  <input type="radio" id="rating2" name="rating" value="2" onClick={() => setRating(1)} /><label for="rating2" title="1 star"></label>
-                  <input type="radio" id="rating1" name="rating" value="1" onClick={() => setRating(0.5)} /><label class="half" for="rating1" title="0.5 star"></label>
-
+                  <input
+                    type="radio"
+                    id="rating10"
+                    name="rating"
+                    value="10"
+                    onClick={() => setRating(5)}
+                  />
+                  <label for="rating10" title="5 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating9"
+                    name="rating"
+                    value="9"
+                    onClick={() => setRating(4.5)}
+                  />
+                  <label class="half" for="rating9" title="4.5 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating8"
+                    name="rating"
+                    value="8"
+                    onClick={() => setRating(4)}
+                  />
+                  <label for="rating8" title="4 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating7"
+                    name="rating"
+                    value="7"
+                    onClick={() => setRating(3.5)}
+                  />
+                  <label class="half" for="rating7" title="3.5 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating6"
+                    name="rating"
+                    value="6"
+                    onClick={() => setRating(3)}
+                  />
+                  <label for="rating6" title="3 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating5"
+                    name="rating"
+                    value="5"
+                    onClick={() => setRating(2.5)}
+                  />
+                  <label class="half" for="rating5" title=" 2.5stars"></label>
+                  <input
+                    type="radio"
+                    id="rating4"
+                    name="rating"
+                    value="4"
+                    onClick={() => setRating(2)}
+                  />
+                  <label for="rating4" title="2 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating3"
+                    name="rating"
+                    value="3"
+                    onClick={() => setRating(1.5)}
+                  />
+                  <label class="half" for="rating3" title="1.5 stars"></label>
+                  <input
+                    type="radio"
+                    id="rating2"
+                    name="rating"
+                    value="2"
+                    onClick={() => setRating(1)}
+                  />
+                  <label for="rating2" title="1 star"></label>
+                  <input
+                    type="radio"
+                    id="rating1"
+                    name="rating"
+                    value="1"
+                    onClick={() => setRating(0.5)}
+                  />
+                  <label class="half" for="rating1" title="0.5 star"></label>
                 </fieldset>
-
-
-
               </>
             </div>
             <div className="flex justify-center p-3">
-
               <p>Your Rating Value is {Rating} </p>
             </div>
 
             <div className="flex justify-between">
               <button
                 onClick={() => {
-                  updateUserRating(username, Rating); // Update or set user rating
-                  // setshowRating(false);
+                  console.log("userRating", Rating, aID);
+                  dispatch(rate(aID.id, Rating));
+                  setRating(0);
+                  setshowRating(false);
+                  appointments[aID.index].rate=true;
                 }}
                 className="px-2 py-2 bg-[#3085d6]  text-white rounded"
               >
@@ -251,6 +329,7 @@ const TableBody = ({
               <button
                 onClick={() => {
                   setshowRating(false);
+                  setRating(0);
                 }}
                 id="container1"
                 className="px-2 py-2 bg-gray-700 text-white rounded"
--- fyp-project_orignal/src/redux/actions/docProfile.js
+++ fyp-project_orignal/src/redux/actions/docProfile.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Action types
-export const GET_DOCTOR_PROFILE = 'GET_DOCTOR_PROFILE';
-export const DOCTOR_PROFILE_SUCCESS = 'DOCTOR_PROFILE_SUCCESS';
-export const DOCTOR_PROFILE_FAILURE = 'DOCTOR_PROFILE_FAILURE';
+export const GET_DOCTOR_PROFILE = "GET_DOCTOR_PROFILE";
+export const DOCTOR_PROFILE_SUCCESS = "DOCTOR_PROFILE_SUCCESS";
+export const DOCTOR_PROFILE_FAILURE = "DOCTOR_PROFILE_FAILURE";
 
 // Action creators
 export const getDoctorProfileRequest = () => ({
@@ -23,67 +23,88 @@ export const doctorProfileFailure = (error, statusCode) => ({
 });
 
 // Thunk to get doctor profile
-export const getProfile = (user,username) => {
+export const getProfile = (user, username) => {
   return async (dispatch) => {
     dispatch(getDoctorProfileRequest());
     let token = localStorage.getItem(user);
     const parsedtoken = token ? JSON.parse(token) : [];
     try {
-      const response = await axios.get(`http://localhost:5000/profile/${user}/${username}`, {
-        headers: {
-          Authorization: `Bearer ${parsedtoken}`,
-        },
-      });
+      const response = await axios.get(
+        `http://localhost:5000/profile/${user}/${username}`,
+        {
+          headers: {
+            Authorization: `Bearer ${parsedtoken}`,
+          },
+        }
+      );
       const jsonResponse = response.data;
       dispatch(doctorProfileSuccess(jsonResponse, 200));
-
     } catch (error) {
       if (error.response) {
         const { status } = error.response;
         const message = error.response.data;
 
-        if (status === 404||status === 422) {
+        if (status === 404 || status === 422) {
           dispatch(doctorProfileFailure(message, status));
         } else {
-          dispatch(doctorProfileFailure(`Error: ${status} Message:${message}`, status));
+          dispatch(
+            doctorProfileFailure(`Error: ${status} Message:${message}`, status)
+          );
         }
       } else {
-        dispatch(doctorProfileFailure('Network error'));
+        dispatch(doctorProfileFailure("Network error"));
       }
     }
   };
 };
 
 // Thunk to register doctor profile
-export const registerDoctorProfile = (user,credentials) => {
+export const registerDoctorProfile = (user, credentials) => {
   return async (dispatch) => {
     // Dispatch an action indicating the start of the registration process
     dispatch(getDoctorProfileRequest());
     let token = localStorage.getItem(`${user}Address`);
     const parsedtoken = token ? JSON.parse(token) : [];
-    credentials.account=parsedtoken;
+    credentials.account = parsedtoken;
 
     try {
       // Make a POST request to register the doctor profile
-      const response = await axios.post(`http://localhost:5000/profile/${user}/register`, credentials);
+      const response = await axios.post(
+        `http://localhost:5000/profile/${user}/register`,
+        credentials
+      );
 
       // Handle the successful registration
       const jsonResponse = response.data;
       dispatch(doctorProfileSuccess(jsonResponse, 201));
       return 201;
-
     } catch (error) {
       // Handle registration failure
       if (error.response) {
         const { status } = error.response;
         const message = error.response.data.error;
 
-        dispatch(doctorProfileFailure(`Error: ${status} Message:${message}`, status));
-        return status
+        dispatch(
+          doctorProfileFailure(`Error: ${status} Message:${message}`, status)
+        );
+        return status;
       } else {
-        dispatch(doctorProfileFailure('Network error',500));
-        return 500
+        dispatch(doctorProfileFailure("Network error", 500));
+        return 500;
       }
+    }
+  };
+};
+export const rate = (_id, value) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(`http://localhost:5000/profile/rate`, {
+        _id,
+        value,
+      });
+      const jsonResponse = response.data;
+    } catch (error) {
+      console.error(error);
     }
   };
 };
